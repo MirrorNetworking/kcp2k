41e1b64caae018150dc72b1ab5de155761a6b2d2
vis2k <info@noobtuts.com>
2020-10-14 10:26:53 +0200

    empty TestClient, TestServer

---
+++ kcp2k/Assets/Scene/TestClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace kcp2k.Examples
+{
+    public class TestClient : MonoBehaviour
+    {
+        // configuration
+        public ushort Port = 7777;
+
+        public void Connect(string ip)
+        {
+        }
+
+        public void Send(ArraySegment<byte> segment)
+        {
+        }
+
+        public void Disconnect()
+        {
+        }
+
+        // MonoBehaviour ///////////////////////////////////////////////////////
+        public void LateUpdate()
+        {
+        }
+    }
+}
---
+++ kcp2k/Assets/Scene/TestServer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace kcp2k.Examples
+{
+    public class TestServer : MonoBehaviour
+    {
+        // configuration
+        public ushort Port = 7777;
+
+        public void StartServer()
+        {
+        }
+
+        public void Send(int connectionId, ArraySegment<byte> segment)
+        {
+        }
+
+        public bool Disconnect(int connectionId)
+        {
+            return false;
+        }
+
+        public string GetAddress(int connectionId)
+        {
+            return "";
+        }
+
+        public void StopServer()
+        {
+        }
+
+        // MonoBehaviour ///////////////////////////////////////////////////////
+        public void LateUpdate()
+        {
+        }
+    }
+}

06bbdf79567d48813802c6bb20a86a6b01c688db
vis2k <info@noobtuts.com>
2020-10-15 18:08:01 +0200

    WND_RCV reset to original C value

--- kcp2k/Assets/kcp2k/kcp/Kcp.cs
+++ kcp2k/Assets/kcp2k/kcp/Kcp.cs
@@ -13,13 +13,13 @@ namespace kcp2k
         public const int RTO_DEF = 200;        // default RTO
         public const int RTO_MAX = 60000;      // maximum RTO
         public const int CMD_PUSH = 81;        // cmd: push data
-        public const int CMD_ACK = 82;         // cmd: ack
+        public const int CMD_ACK  = 82;        // cmd: ack
         public const int CMD_WASK = 83;        // cmd: window probe (ask)
         public const int CMD_WINS = 84;        // cmd: window size (tell)
         public const int ASK_SEND = 1;         // need to send CMD_WASK
         public const int ASK_TELL = 2;         // need to send CMD_WINS
         public const int WND_SND = 32;         // defualt send window
-        public const int WND_RCV = 32;         // default receive window. must be >= max fragment size
+        public const int WND_RCV = 128;        // default receive window. must be >= max fragment size
         public const int MTU_DEF = 1200;       // default MTU
         public const int ACK_FAST = 3;
         public const int INTERVAL = 100;

5a2b25d7818510bb793d43a67e8369721d50545b
vis2k <info@noobtuts.com>
2020-10-18 20:27:34 +0200

    ByteBufferTests: WriteBytesWithResize

--- kcp2k/Assets/Tests/Editor/ByteBufferTests.cs
+++ kcp2k/Assets/Tests/Editor/ByteBufferTests.cs
@@ -25,5 +25,23 @@ namespace kcp2k.Tests
             buffer.WriteBytes(bytes, 2, 2);
             Assert.That(buffer.Position, Is.EqualTo(2));
         }
+
+        // writing more than initial capacity should resize automatically
+        [Test]
+        public void WriteBytesWithResize()
+        {
+            // create array with unique values
+            byte[] bytes = new byte[ByteBuffer.InitialCapacity * 2];
+            for (int i = 0; i < bytes.Length; ++i)
+                bytes[i] = (byte)(i & 0xFF);
+
+            // write
+            buffer.WriteBytes(bytes, 0, bytes.Length);
+            Assert.That(buffer.Position, Is.EqualTo(bytes.Length));
+
+            // compare
+            for (int i = 0; i < bytes.Length; ++i)
+                Assert.That(bytes[i], Is.EqualTo(buffer.RawBuffer[i]));
+        }
     }
 }

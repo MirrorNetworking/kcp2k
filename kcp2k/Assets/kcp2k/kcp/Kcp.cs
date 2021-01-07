daa55f2b1caee8ef8a545d2650b037a8fb04e5a0
vis2k <info@noobtuts.com>
2021-01-07 19:44:54 +0800

    add snd_una comment

--- kcp2k/Assets/kcp2k/kcp/Kcp.cs
+++ kcp2k/Assets/kcp2k/kcp/Kcp.cs
@@ -44,7 +44,7 @@ namespace kcp2k
         readonly uint conv;          // conversation
         internal uint mtu;
         internal uint mss;           // maximum segment size := MTU - OVERHEAD
-        internal uint snd_una;       // unacknowledged
+        internal uint snd_una;       // unacknowledged. e.g. snd_una is 9 it means 8 has been confirmed, 9 and 10 have been sent
         internal uint snd_nxt;
         internal uint rcv_nxt;
         internal uint ssthresh;      // slow start threshold

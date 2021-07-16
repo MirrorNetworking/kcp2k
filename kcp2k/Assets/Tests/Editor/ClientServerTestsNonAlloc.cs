00df00600f85f001e8c4a48435f684bec1b665cc
vis2k <info@noobtuts.com>
2021-07-16 20:13:15 +0800

    perf: where-allocation KcpServer/Client/Connection NonAlloc versions

---
+++ kcp2k/Assets/Tests/Editor/ClientServerTestsNonAlloc.cs
@@ -0,0 +1,34 @@
+// where-allocation version for client server tests
+namespace kcp2k.Tests
+{
+    public class ClientServerTestsNonAlloc : ClientServerTests
+    {
+        protected override void CreateServer()
+        {
+            server = new KcpServerNonAlloc(
+                (connectionId) => {},
+                ServerOnData,
+                (connectionId) => {},
+                DualMode,
+                NoDelay,
+                Interval,
+                0,
+                true,
+                serverSendWindowSize,
+                serverReceiveWindowSize,
+                Timeout
+            );
+            server.NoDelay = NoDelay;
+            server.Interval = Interval;
+        }
+
+        protected override void CreateClient()
+        {
+            client = new KcpClientNonAlloc(
+                () => {},
+                ClientOnData,
+                () => {}
+            );
+        }
+    }
+}

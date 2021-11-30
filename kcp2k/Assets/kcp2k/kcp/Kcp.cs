ecd8913cd742572c5a31c81c5e48bfc274699c57
vis2k <info@noobtuts.com>
2021-11-30 11:03:39 +0100

    Update kcp2k/Assets/kcp2k/kcp/Kcp.cs

--- kcp2k/Assets/kcp2k/kcp/Kcp.cs
+++ kcp2k/Assets/kcp2k/kcp/Kcp.cs
@@ -523,7 +523,7 @@ namespace kcp2k
                 offset += Utils.Decode8u(data, offset, ref cmd);
                 // IMPORTANT kcp encodes 'frg' as 1 ushort.
                 // so we can only support up to 65355 fragments.
-                // (which limits max message size to around 70,000 KB)
+                // (which limits max message size to around 70 MB)
                 offset += Utils.Decode16U(data, offset, ref frg);
                 offset += Utils.Decode16U(data, offset, ref wnd);
                 offset += Utils.Decode32U(data, offset, ref ts);

a1898dc97c0da5500a00e2f5185200c245ef9e7a
vis2k <info@noobtuts.com>
2022-11-23 01:26:09 +0100

    readonly

--- kcp2k/Assets/kcp2k/highlevel/NonAlloc/KcpServerConnectionNonAlloc.cs
+++ kcp2k/Assets/kcp2k/highlevel/NonAlloc/KcpServerConnectionNonAlloc.cs
@@ -8,7 +8,7 @@ namespace kcp2k
 {
     public class KcpServerConnectionNonAlloc : KcpServerConnection
     {
-        IPEndPointNonAlloc reusableSendEndPoint;
+        readonly IPEndPointNonAlloc reusableSendEndPoint;
 
         public KcpServerConnectionNonAlloc(Socket socket, EndPoint remoteEndpoint, IPEndPointNonAlloc reusableSendEndPoint, bool noDelay, uint interval = Kcp.INTERVAL, int fastResend = 0, bool congestionWindow = true, uint sendWindowSize = Kcp.WND_SND, uint receiveWindowSize = Kcp.WND_RCV, int timeout = DEFAULT_TIMEOUT, uint maxRetransmits = Kcp.DEADLINK)
             : base(socket, remoteEndpoint, noDelay, interval, fastResend, congestionWindow, sendWindowSize, receiveWindowSize, timeout, maxRetransmits)
